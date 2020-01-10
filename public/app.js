a4b61f0e08f38558c0afbf6ca07b1dd5af8fa01f
DavydovDenys <denvanrain@gmail.com>
2020-01-10 14:49:42 +0200

    add function total_elements for enumerate elements in cart

--- public/app.js
+++ public/app.js
@@ -6,3 +6,14 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 }
+
+function total_elements()
+{
+	var total = 0
+	for (var i = 0; i < localStorage.length; i++)
+	{
+    	var x = localStorage.getItem(localStorage.key(i));
+    	total = x * 1 + total;
+	}
+	alert('Total products:' + total);
+}
